ef1bf39934ff7b57e0114b858b3bf703fa02d3ab
AGR System <AGR System@AGRsystem>
2017-01-06 21:49:42 -0200

    Ate a pagina de logim pronta

--- AGRradio/AGRradio/src/main/java/br/com/agrsystem/controller/ListaClinicaBean.java
+++ AGRradio/AGRradio/src/main/java/br/com/agrsystem/controller/ListaClinicaBean.java
@@ -10,25 +10,19 @@ import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
-@ManagedBean(name = "loginBean")
+@ManagedBean(name = "listaClinicaBean")
 @RequestScoped
-public class LoginBean implements Serializable {
+public class ListaClinicaBean implements Serializable {
 
     private static final long serialVersionUID = 1L;
     private String erro = null;
     
-    public LoginBean() {    }
+    public ListaClinicaBean() {    }
 
     public String confereSenhaClinica() {
         System.out.println("ConfereSenhaClinica");
         setErro(null);
         return "/clinicas/listaClinica.html";
-    }
-
-    public String confereSenhaMedico() {
-        System.out.println("ConfereSenhaMedico");
-        setErro(null);
-        return "/medicos/listaMedico.html";
     }
 
 
