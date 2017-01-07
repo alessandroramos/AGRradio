834cfba049277911937bc0b559e36301425d066e
AGR System <AGR System@AGRsystem>
2017-01-07 11:31:20 -0200

    ListaClienteBean

--- AGRradio/AGRradio/src/main/java/br/com/agrsystem/controller/ListaClinicaBean.java
+++ AGRradio/AGRradio/src/main/java/br/com/agrsystem/controller/ListaClinicaBean.java
@@ -6,7 +6,18 @@
 
 package br.com.agrsystem.controller;
 
+import br.com.agrsystem.model.dao.HibernateDAO;
+import br.com.agrsystem.model.dao.InterfaceDAO;
+import br.com.agrsystem.model.entities.StatusLaudo;
+import br.com.agrsystem.util.FacesContextUtil;
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
 import java.io.Serializable;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
@@ -17,14 +28,24 @@ public class ListaClinicaBean implements Serializable {
     private static final long serialVersionUID = 1L;
     private String erro = null;
     
+    private String listaStatusLaudo; 
+    private Map<String,String> listaStatusLaudos;
+  
+    private StatusLaudo statusLaudo = new StatusLaudo();
+    private List<StatusLaudo> statusLaudos;
+    
+    
     public ListaClinicaBean() {    }
-
-    public String confereSenhaClinica() {
-        System.out.println("ConfereSenhaClinica");
-        setErro(null);
-        return "/clinicas/listaClinica.html";
+    
+    private InterfaceDAO<StatusLaudo> statusLaudoDAO() {
+        InterfaceDAO<StatusLaudo> statusLaudoDAO = new HibernateDAO<StatusLaudo>(StatusLaudo.class, FacesContextUtil.getRequestSession());
+        return statusLaudoDAO;
     }
-
+    
+    
+    
+    
+    
 
     public String getErro() {
         return erro;
@@ -34,6 +55,69 @@ public class ListaClinicaBean implements Serializable {
     public void setErro(String erro) {
         this.erro = erro;
     }
+    
+    
+@PostConstruct
+    public void init() {
+        setListaStatusLaudos(new HashMap<String, String>());
+        getListaStatusLaudos().put("USAggttt", "USA");
+        getListaStatusLaudos().put("Germany", "Germany");
+        getListaStatusLaudos().put("Brazil ggg", "Brazil");
+         
+    }  
+
+    public void displayLocation() {
+        FacesMessage msg;
+        
+            msg = new FacesMessage("Selected of " + listaStatusLaudo);
+       
+             
+        FacesContext.getCurrentInstance().addMessage(null, msg);  
+    }
+    
+    public void onListaStatusLaudoChange() {
+        System.out.print("Seleciona Lista. AGR");
+    }
+
+//################################GET SET #############################//
+    public String getListaStatusLaudo() {
+        return listaStatusLaudo;
+    }
+
+    public void setListaStatusLaudo(String listaStatusLaudo) {
+        this.listaStatusLaudo = listaStatusLaudo;
+    }
+
+    public Map<String, String> getListaStatusLaudos() {
+        return listaStatusLaudos;
+    }
+
+    public void setListaStatusLaudos(Map<String, String> listaStatusLaudos) {
+        this.listaStatusLaudos = listaStatusLaudos;
+    }
+
+    
+    
+//##########################################Laudos    
+    public StatusLaudo getStatusLaudo() {
+        return statusLaudo;
+    }
+
+    public void setStatusLaudo(StatusLaudo statusLaudo) {
+        this.statusLaudo = statusLaudo;
+    }
+
+    public List<StatusLaudo> getStatusLaudos() {
+        statusLaudos = statusLaudoDAO().getEntities();
+        return statusLaudos;
+    }
+
+    public void setStatusLaudos(List<StatusLaudo> statusLaudos) {
+        this.statusLaudos = statusLaudos;
+    }
+    
+    
+    
     
         
 }
